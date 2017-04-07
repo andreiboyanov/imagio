b3a0e84c181757452be25a26858605c99ac1662c
Andrei Boyanov <andrei.boyanov@gmail.com>
2017-04-07 17:27:58 +0200

    Started xyz from depth calculations

--- imagio/point_cloud_window.cpp
+++ imagio/point_cloud_window.cpp
@@ -6,45 +6,112 @@ namespace imagio
 
 void point_cloud_window::open_json(std::string filename)
 {
-    std::ifstream input(filename);
-    input >> depth_stream;
-    current_frame = 0;
-    show_current_frame();
+	std::ifstream input(filename);
+	input >> depth_stream;
+	current_frame = 0;
+	show_current_frame();
+}
+
+
+//def build_2d_to_3d_matrix(self) :
+//	distortion_model = self.camera_configuration.distortion_model
+//	pinhole_model = self.camera_configuration.pinhole_model
+//	for x in range(0, self.stream_configuration.width) :
+//		for y in range(0, self.stream_configuration.height) :
+//			x1 = (x - pinhole_model.cx) / distortion_model.fx
+//			y1 = (pinhole_model.cy - y) / distortion_model.fy
+//
+//			r2 = x1 * x1 + y1 * y1
+//
+//			k = self.get_nearest_less_k(r2)
+//
+//			elementValues = []
+//			elementValues.append(x1 * k / 1000)
+//			elementValues.append(1 / 1000)
+//			elementValues.append(y1 * k / 1000)
+//
+//			self.brownRadial2Dto3DMatrix[y, x] = np.array(elementValues)
+//
+//			def build_brown_radial_table(self) :
+//			self.kValues = self.buildKValues()
+//			self.brownRadial2Dto3DMatrix = np.empty((self.stream_configuration.height,
+//													 self.stream_configuration.width, 3))
+//			self.build_2d_to_3d_matrix()
+//
+//			def get_nearest_less_k(self, key) :
+//
+//			key = self.create_dict_key(key)
+//
+//			index = self.kValues.bisect(key)
+//			if index == self.brown_radial_lut_size :
+//				index = index - 1
+//				key = self.kValues.iloc[index]
+//				return self.kValues[key]
+//
+//				def create_dict_key(self, value) :
+//				return int(round(value, 3) * 1000)
+//
+//				def transform2dTo3dBrownRadial(self, x, y, depth) :
+//				pixelValues = self.brown_radial_lut[x, y]
+//				x1 = pixelValues[0]
+//				y1 = pixelValues[1]
+//				k = pixelValues[2]
+//
+//				x1 = x1 * k
+//				y1 = y1 * k
+//
+//				return np.array([x1 * depth, y1 * depth, depth])
+
+
+
+void point_cloud_window::calculate_xyz_from_depth(float& x, float& y, float& z, int depth)
+{
+
+}
+
+void point_cloud_window::create_points_from_depth_image()
+{
+	auto stream = skv_file.get_stream_by_name("depth_0");
+
+	assert(stream.get_type() == skv_stream_type_image);
+	assert(stream.get_image_type() == skv_image_type_int16);
+	if(!(stream.has_pinhole_model() && stream.has_distortion_model()))
+	{
+		std::cout << "Pinhole and/or distortion models missing in the skv file.";
+		std::cout << " They are needed for depth 3d points calkculation. " << std::endl;
+		return;
+	}
+
+	painter->init_scene();
+	std::tuple<uint32_t, uint32_t> depth_resolution = stream.get_resolution();
+	image_width = std::get<0>(depth_resolution);
+	image_height = std::get<1>(depth_resolution);
+
+	size_t byte_count = stream.get_frame_byte_count(current_frame);
+	std::vector<uint16_t> data(byte_count / sizeof(uint16_t));
+	stream.get_frame_data(current_frame, data);
+	for(int image_x = 0; image_x < image_width; image_x++)
+	{
+		for(int image_y = 0; image_y < image_height; image_y++)
+		{
+			int image_index = image_x * image_width + image_y;
+			int depth = (int)data[image_index];
+			float x, y, z;
+			calculate_xyz_from_depth(x, y, z, depth);
+		}
+	}
+
 }
 
 void point_cloud_window::open_skv_depth(std::string filename)
 {
 	try
 	{
-		auto file = softkinetic::skv::open_file(filename);
+		skv_file = softkinetic::skv::open_file(filename);
 
 		std::string vendor_name, camera_model;
-		std::tie(vendor_name, camera_model) = file.get_device_info();
-		auto& stream = file.get_stream_by_name("depth_0");
-
-		assert(stream.get_type() == skv_stream_type_image);
-		assert(stream.get_image_type() == skv_image_type_int16);
-
-	 // if(stream.has_pinhole_model())
-	 // {
-	 // 	skv_pinhole_model pinhole = stream.get_pinhole_model();
-	 // }
-	 // if(stream.has_distortion_model())
-	 // {
-	 // 	skv_distortion_model distortion = stream.get_distortion_model();
-	 // }
-	 // if(stream.has_stereo_transform())
-	 // {
-	 // 	skv_stereo_transform stereo = stream.get_stereo_transform();
-	 // }
-
-		painter->init_scene();
-		std::tuple<uint32_t, uint32_t> depth_resolution = stream.get_resolution();
-		image_width = std::get<0>(depth_resolution);
-
-		size_t byte_count = stream.get_frame_byte_count(current_frame);
-		std::vector<uint16_t> data(byte_count / sizeof(uint16_t));
-		stream.get_frame_data(current_frame, data);
+		std::tie(vendor_name, camera_model) = skv_file.get_device_info();
+		create_points_from_depth_image();
 	}
 	catch(softkinetic::skv::file_error& e)
 	{
@@ -62,35 +129,35 @@ void point_cloud_window::open_skv_depth(std::string filename)
 
 void point_cloud_window::move_forward()
 {
-    current_frame++;
-    if (current_frame >= (int)depth_stream.size()) current_frame = depth_stream.size() - 1;
-    std::cout << "frames = " << depth_stream.size() << "; current_frame = " << current_frame << std::endl;
-    show_current_frame();
+	current_frame++;
+	if(current_frame >= (int)depth_stream.size()) current_frame = depth_stream.size() - 1;
+	std::cout << "frames = " << depth_stream.size() << "; current_frame = " << current_frame << std::endl;
+	show_current_frame();
 }
 
 void point_cloud_window::move_backward()
 {
-    current_frame--;
-    if(current_frame < 0) current_frame = 0;
-    show_current_frame();
+	current_frame--;
+	if(current_frame < 0) current_frame = 0;
+	show_current_frame();
 }
 
 void point_cloud_window::show_current_frame()
 {
-    float scale = 1100.0f;
-    painter->clear();
-    std::cout << "Draw balls for frame " << current_frame << std::endl;
-    ImColor ball_color = ImColor(255, 100, 100);
-    for (int i = 0; i < 9; i++)
-    {
-        painter->draw_point(100.0f * i / scale, 100.0f * current_frame / scale, 0.0f, ball_color, 20.0f);
-    }
-    std::cout << "Showing frame " << current_frame << std::endl;
+	float scale = 1100.0f;
+	painter->clear();
+	std::cout << "Draw balls for frame " << current_frame << std::endl;
+	ImColor ball_color = ImColor(255, 100, 100);
+	for(int i = 0; i < 9; i++)
+	{
+		painter->draw_point(100.0f * i / scale, 100.0f * current_frame / scale, 0.0f, ball_color, 20.0f);
+	}
+	std::cout << "Showing frame " << current_frame << std::endl;
 	unsigned int count = 0;
 	ImColor point_color = ImColor(100, 100, 100);
 	for(int i = 0; i < (int)depth_stream[current_frame].size() &&
-                      count < painter->get_max_vertices();
-        i += 3)
+		count < painter->get_max_vertices();
+		i += 3)
 	{
 		float x = depth_stream[current_frame][i].asFloat() / scale;
 		float y = depth_stream[current_frame][i + 1].asFloat() / scale;
--- imagio/point_cloud_window.h
+++ imagio/point_cloud_window.h
@@ -20,12 +20,16 @@ class point_cloud_window : public wimgui::paint_window
 {
 private:
     Json::Value depth_stream;
+	softkinetic::skv::file skv_file;
     int current_frame = 0;
     int image_width = 320;
+    int image_height = 240;
 public:
 	point_cloud_window(const char* _title) : paint_window(_title) {}
 	void open_json(std::string filename);
 	void open_skv_depth(std::string filename);
+	void create_points_from_depth_image();
+	void calculate_xyz_from_depth(float& x, float &y, float &z, int depth);
     void move_forward();
     void move_backward();
     void show_current_frame();
