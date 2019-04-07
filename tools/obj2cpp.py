4e2df716784cff93fd2d94e0307169a9014baa0e
Andrei Boyanov <andrei.boyanov@gmail.com>
2019-04-07 10:39:28 +0200

    The conversion script done.
    Still need to be tested the generated code but given a cube .obj file
    the script is generating quite nice .h and .cpp file

--- tools/obj2cpp.py
+++ tools/obj2cpp.py
@@ -3,6 +3,31 @@ import sys
 import argparse
 
 
+def parse_2d_line(line):
+    x, y = line.strip().split(" ")
+    return float(x), float(y)
+
+
+def parse_3d_line(line):
+    x, y, z = line.strip().split(" ")
+    return float(x), float(y), float(z)
+
+
+def parse_triangle_line(line, positions, normals, texture_coordinates):
+    triangle_vertices = list()
+    triangle_normals  = list()
+    triangle_textures = list()
+    for triangle_point in line.strip().split(" "):
+        vertex_index, texture_index, normal_index = triangle_point.split("/")
+        vertex_index = int(vertex_index) - 1
+        triangle_vertices.append(positions[vertex_index])
+        normal_index = int(normal_index) - 1
+        triangle_normals.append(normals[normal_index])
+        texture_index = int(texture_index) - 1
+        triangle_textures.append(texture_coordinates[texture_index])
+    return triangle_vertices, triangle_normals, triangle_textures
+
+
 def main(argv):
     parser = argparse.ArgumentParser(
         description="Convert a Wavefront OBJ file to C code. "
@@ -11,63 +36,112 @@ def main(argv):
                     "The faces must be triangulated."
     )
     parser.add_argument(
-        "input-file",
+        "input_file",
         help="The input .obj file"
     )
     parser.add_argument(
-        "output_folder",
-        required=False,
+        "-o",
+        "--output-folder",
         help="The folder to store the converted files in. "
              "Defaults to the input file's folder."
     )
 
     args = parser.parse_args(argv)
-    vertice_lines = list()
-    normal_lines = list()
-    uv_lines = list()
-    face_lines = list()
+    positions = list()
+    normals = list()
+    texture_mapping = list()
+    faces = dict(
+        vertices = list(),
+        normals = list(),
+        texture_mapping = list()
+    )
     with open(args.input_file) as obj_file:
         for line in obj_file.readlines():
             if line.startswith("v "):
-                vertice_lines.append(line[2: ]).strip()
+                position = parse_3d_line(line[2: ])
+                positions.append(position)
             elif line.startswith("vt "):
-                uv_lines.append(line[3: ]).strip()
+                texture_coordinates = parse_2d_line(line[3: ])
+                texture_mapping.append(texture_coordinates)
             elif line.startswith("vn "):
-                normal_lines.append(line[3: ]).strip()
+                normal = parse_3d_line(line[3: ])
+                normals.append(normal)
             elif line.startswith("f "):
-                face_lines.append(line[2: ]).strip()
+                triangle_vertices, triangle_normals, triangle_texture_coordinates = parse_triangle_line(
+                    line[2:], positions, normals, texture_mapping
+                )
+                faces["vertices"].append(triangle_vertices)
+                faces["normals"].append(triangle_normals)
+                faces["texture_mapping"].append(triangle_texture_coordinates)
+
     output_folder = args.output_folder or os.path.dirname(args.input_file)
     mesh_name = os.path.splitext(os.path.basename(args.input_file))[0]
-    with open(os.path.join(output_folder, mesh_name + ".h"), "w+") as h_file:
-        position_count = len(vertice_lines)
-        normal_count = len(normal_lines)
-        uv_mapping_count = len(uv_lines)
-        triangle_count = len(face_lines)
-        vertice_count = len(face_lines) * 3
 
+    position_count = len(positions)
+    normal_count = len(normals)
+    uv_mapping_count = len(texture_mapping)
+    triangle_count = len(faces["vertices"])
+    vertice_count = triangle_count * 3
+
+    with open(os.path.join(output_folder, mesh_name + ".h"), "w+") as h_file:
         h_file.write("// Auto-generated file with definition of a {} mesh\n".format(mesh_name))
         h_file.write("// Positions: {}\n".format(position_count))
         h_file.write("// Normals: {}\n".format(normal_count))
         h_file.write("// Texture mappings: {}\n".format(uv_mapping_count))
-        h_file.write("// Triangles: {}\n".format(triangle_count)))
+        h_file.write("// Triangles: {}\n".format(triangle_count))
         h_file.write("// Vertices: {}\n".format(vertice_count))
         h_file.write("\n")
+        h_file.write("namespace imagio\n")
+        h_file.write("{\n")
+        h_file.write("namespace meshes\n")
+        h_file.write("{\n")
+        h_file.write("namespace cube\n")
+        h_file.write("{\n")
         h_file.write("const int vertice_count;\n")
-        h_file.write("const float positions[{}];\n".format(position_count))
-        h_file.write("const float texture_mapping[{}];\n".format(uv_mapping_count)))
-        h_file.write("const float normals[{}];\n".format(normal_count))
-    with open(os.path.join(output_folder, mesh_name + ".c"), "w+") as c_file:
+        h_file.write("const float positions[{}];\n".format(vertice_count * 3))
+        h_file.write("const float texture_mapping[{}];\n".format(vertice_count * 2))
+        h_file.write("const float normals[{}];\n".format(vertice_count * 3))
+        h_file.write("};\n")
+        h_file.write("};\n")
+        h_file.write("};\n")
+            
+    with open(os.path.join(output_folder, mesh_name + ".cpp"), "w+") as c_file:
         c_file.write("// Auto-generated file with definition of a {} mesh\n".format(mesh_name))
         c_file.write('#include "{}.h"\n'.format(mesh_name))
         c_file.write("\n")
-        c_file.write("const int vertice_count = {}\n".format(vertice_count))
-        c_file.write("const float cubePositions[{}] = \n".format(vertice_count * 3))
+        c_file.write("namespace imagio\n")
+        c_file.write("{\n")
+        c_file.write("namespace meshes\n")
+        c_file.write("{\n")
+        c_file.write("namespace cube\n")
+        c_file.write("{\n")
+        c_file.write("const int vertice_count = {};\n".format(vertice_count))
+        c_file.write("const float vertices[{}] = \n".format(vertice_count * 3))
         c_file.write("{\n")
         for i in range(triangle_count):
-            pass
-        c_file.write("\n")
-        c_file.write("\n")
-        c_file.write("\n")
+            vertex = faces["vertices"][i]
+            c_file.write("\t{}, {}, {},\n".format(vertex[0][0], vertex[0][1], vertex[0][2]))
+            c_file.write("\t{}, {}, {},\n".format(vertex[1][0], vertex[1][1], vertex[1][2]))
+            c_file.write("\t{}, {}, {},\n".format(vertex[2][0], vertex[2][1], vertex[2][2]))
+        c_file.write("};\n")
+        c_file.write("const float normals[{}] = \n".format(vertice_count * 3))
+        c_file.write("{\n")
+        for i in range(triangle_count):
+            normal = faces["normals"][i]
+            c_file.write("\t{}, {}, {},\n".format(normal[0][0], normal[0][1], normal[0][2]))
+            c_file.write("\t{}, {}, {},\n".format(normal[1][0], normal[1][1], normal[1][2]))
+            c_file.write("\t{}, {}, {},\n".format(normal[2][0], normal[2][1], normal[2][2]))
+        c_file.write("};\n")
+        c_file.write("const float texture_coordinates[{}] = \n".format(vertice_count * 2))
+        c_file.write("{\n")
+        for i in range(triangle_count):
+            uv = faces["texture_mapping"][i]
+            c_file.write("\t{}, {},\n".format(uv[0][0], uv[0][1]))
+            c_file.write("\t{}, {},\n".format(uv[1][0], uv[1][1]))
+        c_file.write("};\n")
+        c_file.write("};\n")
+        c_file.write("};\n")
+        c_file.write("};\n")
         c_file.write("\n")
         c_file.write("\n")
 
