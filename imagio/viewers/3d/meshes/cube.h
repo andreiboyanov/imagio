c1d08ee783daabcaecb1d0d06d58089b7f61c236
Andrei Boyanov <andrei.boyanov@gmail.com>
2019-04-07 10:40:01 +0200

    The generated cube header and implementation file

---
+++ imagio/viewers/3d/meshes/cube.h
@@ -0,0 +1,20 @@
+// Auto-generated file with definition of a cube mesh
+// Positions: 8
+// Normals: 6
+// Texture mappings: 20
+// Triangles: 12
+// Vertices: 36
+
+namespace imagio
+{
+namespace meshes
+{
+namespace cube
+{
+const int vertice_count;
+const float positions[108];
+const float texture_mapping[72];
+const float normals[108];
+};
+};
+};
